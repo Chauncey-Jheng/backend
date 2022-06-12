d705aad5c234c8ba915911242d5b7b8b778b7115
Chauncey-Jheng <1205532391@qq.com>
2022-06-12 17:04:51 +0800

    add index page

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8-*-
 from flask import url_for, escape
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello():
     return '<h1>hello!</h1>'
 
@@ -18,3 +19,21 @@ def test_url_for():
     print(url_for('test_url_for'))
     print(url_for('test_url_for', num = 2))
     return 'Test page'
+
+name = 'zcxhhh'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html',name=name,movies=movies)
